564021030d122684d4a622c9a24fd5c205987536
sebasdevelopero <121590072+sebasdevelopero@users.noreply.github.com>
2022-12-28 22:55:40 -0300

    initial commit

---
+++ funcioness.js
@@ -0,0 +1,27 @@
+document.getElementById('btnsobremi').addEventListener('click', function(){
+    document.getElementById('sobremi').style.color='blue'
+    document.getElementById('sobremi').style.backgroundColor='yellow'
+    console.log("Se ha resaltado la seccion sobre mi")
+
+});
+
+document.getElementById('btndatosgenerales').addEventListener('click', function(){
+    document.getElementById('datosgenerales').style.color='blue'
+    document.getElementById('datosgenerales').style.backgroundColor='yellow'
+    console.log("Se ha resaltado la seccion datos generales")
+
+});
+
+document.getElementById('btnformacionacademica').addEventListener('click', function(){
+    document.getElementById('formacionacademica').style.color='blue'
+    document.getElementById('formacionacademica').style.backgroundColor='yellow'
+    console.log("Se ha resaltado la seccion formacion academica")
+
+});
+
+document.getElementById('btnexperiencialaboral').addEventListener('click', function(){
+    document.getElementById('experiencialaboral').style.color='blue'
+    document.getElementById('experiencialaboral').style.backgroundColor='yellow'
+    console.log("Se ha resaltado la seccion experiencia laboral")
+
+});
